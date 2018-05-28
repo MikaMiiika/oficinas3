e7a41f8b14157232282ba4a20186011518813087
MikaMiiika <isabellamika@gmail.com>
2018-05-28 00:21:09 -0300

    MongoDB LocalHost problem solved 2

--- pomodb/__init__.py
+++ pomodb/__init__.py
@@ -1,10 +1,11 @@
 from flask_pymongo import PyMongo
 #from flask_mongo_sessions import MongoDBSessionInterface
+import os
 
 from api import *
 
 app.config['MONGO_DBNAME'] = 'smartdb'
-app.config['MONGO_URI'] = 'mongodb://smart:admin@172.30.234.76:27017/smartdb'
+app.config['MONGO_URI'] = 'mongodb://smart:admin@%s:27017/smartdb' % (os.environ['MONGODB_34_RHEL7_PORT_27017_TCP_ADDR'])
 
 mongo = PyMongo(app)
 #with app.app_context():
