b216cd74d78234a5b02a8b0f2a6433bbbf86d04c
MikaMiiika <isabellamika@gmail.com>
2018-05-28 01:12:41 -0300

    Fix path

--- pomodb/__init__.py
+++ pomodb/__init__.py
@@ -1,12 +1,12 @@
 from flask_pymongo import PyMongo
 #from flask_mongo_sessions import MongoDBSessionInterface
-
+import os
 from api import *
 
 app.config['MONGO_DBNAME'] = 'smartdb'
-#app.config['MONGO_URI'] = 'mongodb://smart:admin@%s:27017/smartdb' % (os.environ['MONGODB_34_RHEL7_PORT_27017_TCP_ADDR'])
+app.config['MONGO_URI'] = 'mongodb://smart:admin@%s:27017/smartdb' % (os.environ['MONGODB_34_RHEL7_PORT_27017_TCP_ADDR'])
 
-app.config['MONGO_URI'] = 'mongodb://smart:admin@localhost:27017/smartdb'
+#app.config['MONGO_URI'] = 'mongodb://smart:admin@localhost:27017/smartdb'
 
 mongo = PyMongo(app)
 #with app.app_context():
